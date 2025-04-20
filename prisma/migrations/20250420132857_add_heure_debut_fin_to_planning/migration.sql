719d58d6c14cd2c745354f1eddcb974c83fe43a6
Nairolf13 <fb_du_13@live.fr>
2025-04-20 16:38:59 +0200

    update reset password

---
+++ prisma/migrations/20250420132857_add_heure_debut_fin_to_planning/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `Planning` ADD COLUMN `heureDebut` VARCHAR(191) NULL,
+    ADD COLUMN `heureFin` VARCHAR(191) NULL;
---
+++ prisma/migrations/20250420134146_moment_optional/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Planning` MODIFY `moment` VARCHAR(191) NULL;
