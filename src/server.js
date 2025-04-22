adb5f79492f341e51c270538de6ac03cd2e3b0a2
Nairolf13 <fb_du_13@live.fr>
2025-04-22 13:10:31 +0200

    add task on sidebar

--- src/server.js
+++ src/server.js
@@ -3,8 +3,6 @@ const http = require('http').createServer(app);
 const { initSocket } = require('./utils/socket');
 require('dotenv').config();
 
-// app.js gère déjà cors et express.json
-
 initSocket(http);
 
 const PORT = process.env.PORT || 3001;
