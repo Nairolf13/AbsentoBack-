3b784901d47f4eb3ea8d8e729cda9ef7725abbf1
Nairolf13 <fb_du_13@live.fr>
2025-04-24 23:19:45 +0200

    update process.env

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000/api" ,   // ou ton backend
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3000/api",   // ou ton backend
   withCredentials: true,
 });
